f91750485cabcad986442595b9fb3bedeaf02962
cK1995 <kulkarni.chinmayrules@gmail.com>
2021-12-07 17:32:16 -0500

    Added new changes

--- filesize.c
+++ filesize.c
@@ -9,21 +9,23 @@ int main(int argc, char* argv[]){
 	unsigned int result = 0;
 	int fd = open(argv[1], 0), n;
 	char buffer[buffer_size];
+	int fullFileRead = 1;
 	while((n = read(fd, buffer, sizeof(buffer))) > 0){
 		bin = clock() - start;
 		msec2 = bin * 1000 / CLOCKS_PER_SEC;
-		if (msec2 > 10) {
-			printf("The number of blocks read are %d in 10 ms\n",block_count);
-			printf("The memory read in 100 ms is %d bytes\n",block_count*buffer_size);
+		if (msec2 > 5000) {
+			printf("The number of blocks read are %d in 5 seconds\n",block_count);
+			printf("The memory read in 5 seconds is %d bytes\n",block_count*buffer_size);
+			fullFileRead = 0;
 			break;
 		}
 		block_count++;
 	}
 	close(fd);
-	/*diff = clock() - start;
-	msec = diff * 1000 / CLOCKS_PER_SEC;
-	printf("The number of blocks are %d",block_count);
-	printf("Time taken %d seconds %d milliseconds\n", msec/1000, msec%1000);
-	printf("XOR result is %d", result);*/
+	if (fullFileRead == 1) {
+		diff = clock() - start;
+		msec = diff * 1000 / CLOCKS_PER_SEC;
+		printf("The entire file has been read in %d seconds and %d milliseconds", msec/1000, msec%1000);
+	}
 	return 0;
 }
